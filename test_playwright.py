cde53f80ae88ebe7ed25ab1bea702b66b62b781e
kakakakakku <y.yoshida22@gmail.com>
2022-11-30 13:48:38 +0900

    Updated test_playwright.py with error

--- test_playwright.py
+++ test_playwright.py
@@ -5,4 +5,4 @@ from playwright.sync_api import Page, expect
 
 def test_playwright(page: Page):
     page.goto('https://playwright.dev/')
-    expect(page).to_have_title(re.compile('Playwright'))
+    expect(page).to_have_title(re.compile('Playwright2'))
